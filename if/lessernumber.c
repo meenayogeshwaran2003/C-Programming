7c1c50eb0f53aecd4db8d85c055ec358fcf24f1e
meenayogeshwaran2003 <yw45475@gmail.com>
2024-12-03 09:43:09 +0530

    Add files via upload

---
+++ if/lessernumber.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+	int a=10,b=5,c=23;
+
+	if(a<b)
+        {
+	printf("the lesser number is %d\n",a);
+	}
+	if(b<c)
+	{
+	printf("the lesser number is %d\n",b);
+	}
+	if(c<a)
+	{
+        printf("the lesser number is %d\n",c);
+	}
+	else
+	{
+		printf("the leser number is %d\n",a);
+	}
+	 return 0;
+}
